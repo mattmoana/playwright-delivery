2b8ed1f4cd39b4c7b8e5929f0d714c934b8b832b
mattmoana <matt.morozov@oc.eu>
2024-11-18 12:55:26 +0200

    yml changes to pipeline

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,52 +1,51 @@
 import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 
-const URL = process.env.APP_URL
+const URL = process.env.APP_URL || 'https://fe-delivery.tallinn-learning.ee/signin';
 
 test.beforeEach(async ({ page }) => {
   await page.goto(URL);
+});
 
+test('Check for incorrect credentials message and close pop up message', async ({ page }) => {
+  const randomUsername = faker.internet.username();
+  const randomPassword = faker.internet.password();
 
-  test('Check for incorrect credentials message and close pop up message', async ({page}) => {
-    const randomUsername = faker.internet.username()
-    const randomPassword = faker.internet.password();
+  const usernameField = page.getByTestId("username-input");
+  const passwordField = page.getByTestId("password-input");
+  const signInButton = page.getByTestId("signIn-button");
+  const errorPopUpMessage = page.getByTestId("authorizationError-popup");
+  const closeButton = page.getByTestId("authorizationError-popup-close-button");
 
-    const usernameField = page.getByTestId("username-input");
-    const passwordField = page.getByTestId("password-input");
-    const signInButton = page.getByTestId("signIn-button");
-    const errorPopUpMessage = page.getByTestId("authorizationError-popup");
-    const closeButton = page.getByTestId("authorizationError-popup-close-button");
+  await usernameField.fill(randomUsername);
+  console.log(randomUsername);
+  await passwordField.fill(randomPassword);
+  console.log(randomPassword);
+  await signInButton.click();
+  await expect(errorPopUpMessage).toBeVisible();
 
-    await usernameField.fill(randomUsername);
-    console.log(randomUsername)
-    await passwordField.fill(randomPassword);
-    console.log(randomPassword)
-    await signInButton.click();
-    await expect(errorPopUpMessage).toBeVisible();
+  await closeButton.click();
+  await expect(signInButton).toBeEnabled();
+});
 
-    await closeButton.click();
-    await expect(signInButton).toBeEnabled();
-  });
+test('Check for error messages for login input', async ({ page }) => {
+  const usernameField = page.getByTestId("username-input");
+  const passwordField = page.getByTestId("password-input");
+  const emptyErrorMessageForUserName = page.getByTestId('username-input-error').nth(0);
+  const emptyErrorMessageForPassword = page.getByTestId('username-input-error').nth(1);
 
-  test('Check for error messages for login input', async ({page}) => {
-    const usernameField = page.getByTestId("username-input");
-    const passwordField = page.getByTestId("password-input");
-    const emptyErrorMessageForUserName = page.getByTestId('username-input-error').nth(0);
-    const emptyErrorMessageForPassword = page.getByTestId('username-input-error').nth(1);
+  const emptyErrorMessageForShortUserName = page.getByText('The field must contain at least of characters: 2');
+  const emptyErrorMessageForShortPassword = page.getByText('The field must contain at least of characters: 8');
 
-    const emptyErrorMessageForShortUserName = page.getByText('The field must contain at least of characters: 2');
-    const emptyErrorMessageForShortPassword = page.getByText('The field must contain at least of characters: 8');
+  await usernameField.fill("t");
+  await expect(emptyErrorMessageForShortUserName).toBeVisible();
 
-    await usernameField.fill("t");
-    await expect(emptyErrorMessageForShortUserName).toBeVisible();
+  await usernameField.fill("");
+  await expect(emptyErrorMessageForUserName).toBeVisible();
 
-    await usernameField.fill("");
-    await expect(emptyErrorMessageForUserName).toBeVisible();
+  await passwordField.fill("test");
+  await expect(emptyErrorMessageForShortPassword).toBeVisible();
 
-    await passwordField.fill("test");
-    await expect(emptyErrorMessageForShortPassword).toBeVisible();
-
-    await passwordField.fill("");
-    await expect(emptyErrorMessageForPassword).toBeVisible();
-  });
-})
+  await passwordField.fill("");
+  await expect(emptyErrorMessageForPassword).toBeVisible();
+});
